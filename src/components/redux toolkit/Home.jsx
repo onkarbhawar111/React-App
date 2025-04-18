0ceab8b92f9e629d40fd33c0e190b25b7fa33a10
onkarbhawar111 <onkarbhawar111@gmail.com>
2025-04-18 13:11:22 +0530

    18-4

--- src/components/redux toolkit/Home.jsx
+++ src/components/redux toolkit/Home.jsx
@@ -11,39 +11,39 @@ const Home = () => {
 
     const dispatch = useDispatch()
 
-    function handleDelete(id){
-dispatch(deleteUser({ id: id }))
+    function handleDelete(id) {
+        dispatch(deleteUser({ id: id }))
     }
 
-  return (
-    <div className='container'>
-        <h2>CRUD App</h2>
-        <Link to='/create' className='btn btn-success my-3'>Create + </Link>
-        <table className='table'>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((user, index)=> (
-                    <tr key={index}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>    
-                        <td>{user.email}</td>
-                        <td>
-                            <Link to={`/edit/${user.id}`} className='btn btn-sm btn-primary'>Edit</Link>
-                            <button onClick={() => handleDelete(user.id)} className='btn btn-sm btn-danger ms-2'>Delete</button>
-                        </td>
+    return (
+        <div className='container'>
+            <h2>CRUD App</h2>
+            <Link to='/create' className='btn btn-success my-3'>Create + </Link>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-  )
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <Link to={`/edit/${user.id}`} className='btn btn-sm btn-primary'>Edit</Link>
+                                <button onClick={() => handleDelete(user.id)} className='btn btn-sm btn-danger ms-2'>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import App1 from './components/Role Based Auth/App1.jsx'
+// import App1 from './components/Role Based Auth/App1.jsx'
 import App2 from './App2.jsx'
 import App3 from './components/App3.jsx'
 import { store } from './components/redux/store.jsx'
@@ -13,13 +13,15 @@ import TailWindCSS from './components/TailWindCSS.jsx'
 import NavBar from './components/NavBar.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TypesExample from './components/Button.jsx'
+import App1 from './components/redux toolkit/App1.jsx'
 
 createRoot(document.getElementById('root')).render(
-    // <Provider store={store}>
     //     <Appfinal />
-    // </Provider>,
     // <Pagination />
     // <TailWindCSS />
     // <NavBar />
-    <TypesExample />
+    // <TypesExample />
+    <Provider store={store}>
+    <App1 />
+     </Provider>,
 )
